691696af4b191cba411f79cdea823c44e7a4e6bf
huangboju <bhuang@xiaohongshu.com>
2019-01-04 12:48:14 +0800

    整理代码

--- TangramUI/Wallet/StickCollectionViewFlowLayout/StickCollectionViewFlowLayout.swift
+++ TangramUI/Wallet/StickCollectionViewFlowLayout/StickCollectionViewFlowLayout.swift
@@ -14,19 +14,19 @@ class StickCollectionViewFlowLayout: UICollectionViewFlowLayout {
     
     override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
         guard let oldItems = super.layoutAttributesForElements(in: rect) else { return nil }
-        
         var headerAttributes: UICollectionViewLayoutAttributes?
-    
+        var result: [UICollectionViewLayoutAttributes] = []
         for obj in oldItems {
-            if obj.representedElementKind == UICollectionView.elementKindSectionHeader {
-                headerAttributes = obj
+            guard let arrt = obj.copy() as? UICollectionViewLayoutAttributes else { continue }
+            if arrt.representedElementKind == UICollectionView.elementKindSectionHeader {
+                headerAttributes = arrt
             } else {
-                updateCellAttributes(obj, withSectionHeader: headerAttributes)
-                
+                updateCellAttributes(arrt, withSectionHeader: headerAttributes)
             }
+            result.append(arrt)
         }
 
-        return oldItems
+        return result
     }
     
     private func updateCellAttributes(_ attributes: UICollectionViewLayoutAttributes, withSectionHeader headerAttributes: UICollectionViewLayoutAttributes?) {
@@ -38,13 +38,11 @@ class StickCollectionViewFlowLayout: UICollectionViewFlowLayout {
         let minY = collectionView!.bounds.minY + top
         let maxY = attributes.frame.minY - (headerAttributes?.bounds.height ?? 0)
         let finalY = max(minY, maxY)
-        
-        let deltaY = (finalY - attributes.frame.minY) / attributes.frame.height
-        
+
         if firstItemTransform > 0 {
+            let deltaY = (finalY - attributes.frame.minY) / attributes.frame.height
             attributes.transform = CGAffineTransform(scaleX: 1 - deltaY * firstItemTransform, y: 1 - deltaY * firstItemTransform)
         }
-        
         attributes.frame.origin.y = finalY
         attributes.zIndex = attributes.indexPath.row
         
